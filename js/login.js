9ee6aba420b2d268c9e178ba218dcb4c80c6f61d
TatianeAndrade <tatiane.ferreira@ccc.ufcg.edu.br>
2018-08-01 15:54:11 -0300

    Integrar com Gloogle, index e rotas.

---
+++ js/login.js
@@ -0,0 +1,23 @@
+var googleUser = {};
+  var startApp = function() {
+    gapi.load('auth2', function(){
+      // Retrieve the singleton for the GoogleAuth library and set up the client.
+      auth2 = gapi.auth2.init({
+        client_id: '594906753462-rkhlero91bddrg1kqodqtk7qp4nbglmm.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        // Request scopes in addition to 'profile' and 'email'
+        //scope: 'additional_scope'
+      });
+      attachSignin(document.getElementById('customBtn'));
+    });
+  };
+
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {},
+        function(googleUser) {
+             console.log(googleUser.getAuthResponse().id_token);
+        }, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
