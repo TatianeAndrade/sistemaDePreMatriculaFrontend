e55168f58c8c35dc965743dffcff96b2172a8c68
MARCOS ANTONIO BATISTA DE OLIVEIRA JUNIO 116110176 <marcosaboj@lcc3-017.lcc.ufcg.edu.br>
2018-08-01 17:17:53 -0300

    imprimindo informações.

--- js/login.js
+++ js/login.js
@@ -16,6 +16,11 @@ var googleUser = {};
     console.log(element.id);
     auth2.attachClickHandler(element, {},
         function(googleUser) {
+            var profile = googleUser.getBasicProfile()
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
              console.log(googleUser.getAuthResponse().id_token);
         }, function(error) {
           alert(JSON.stringify(error, undefined, 2));
